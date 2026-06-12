3d2550ece3d246a272e9aa3c9c6c1f306d5a0692
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-06-12 12:50:19 +0300

    style(views): fix spacing issues to comply with PEP8

--- cinema/views.py
+++ cinema/views.py
@@ -86,10 +86,10 @@ class ActorDetail(
         return self.update(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
-        return  self.partial_update(request,*args, **kwargs)
+        return self.partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
-        return  self.destroy(request,*args, **kwargs)
+        return self.destroy(request, *args, **kwargs)
 
 
 class CinemaHallViewSet(
